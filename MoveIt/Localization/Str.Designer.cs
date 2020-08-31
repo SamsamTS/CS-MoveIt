175c02d27c018ed8b36a8ab9d4799ed7a7772f5e
Rob C <github@quboid.net>
2020-08-31 21:30:48 +0100

    Translation WIP

--- MoveIt/Localization/Str.Designer.cs
+++ MoveIt/Localization/Str.Designer.cs
@@ -11,7 +11,6 @@
 namespace MoveIt.Localization {
     using System;
     
-    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
